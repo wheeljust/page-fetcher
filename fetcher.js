3a8aa9da4ca6d004500fbbb773054637654f0038
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-18 03:36:05 +0000

    adds a request call for link provided

--- fetcher.js
+++ fetcher.js
@@ -3,5 +3,7 @@ const request = require('request');
 const link = process.argv[2];
 const fileName = process.argv[3];
 
-
+request(link, (error, response, body) => {
+  console.log(`Downloaded and saved ${body.length} bytes to ${fileName}`);
+});
 

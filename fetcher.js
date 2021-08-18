33e81d7b12e7e5d3ecde8bbf87db275a2104bff9
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-18 21:44:15 +0000

    adds fs writefile to create and save the page content in a new file

--- fetcher.js
+++ fetcher.js
@@ -1,9 +1,17 @@
 const request = require('request');
+const fs = require('fs');
 
 const link = process.argv[2];
 const fileName = process.argv[3];
 
 request(link, (error, response, body) => {
+  fs.writeFile(fileName, body, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+  })
+
   console.log(`Downloaded and saved ${body.length} bytes to ${fileName}`);
 });
 

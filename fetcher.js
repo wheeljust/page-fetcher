b71a759bccb1f383d253757d222b7cc7f8a5b962
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-19 00:46:10 +0000

    added checks for a valid file name and extension

--- fetcher.js
+++ fetcher.js
@@ -2,12 +2,6 @@ const request = require('request');
 const fs = require('fs');
 const readline = require('readline');
 
-//Set up readline to allow userinput for file overwrite
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 //Arguments entered by the user will be in the order of 'node fetcher.js 'link' ./filelocation
 const link = process.argv[2];
 const fileName = process.argv[3];
@@ -35,18 +29,40 @@ const saveFile = () => {
   });
 };
 
+// Check if there is a file name error
+if (!fileName.includes('./')) {
+  console.log("File name error. Missing './' for local drive");
+  // rl.close();
+};
+
+if(!fileName.includes('.html')) {
+  console.log("File name error. Missing '.html' for file extension");
+  // rl.close();
+}
+
 // Check for file name, if found prompt the user to input Y yto overwrite the file.
-fs.access(fileName, fs.constants.R_OK, (err) => {
-  if (!err) {
-    rl.question('This file name already exists. Type Y to overwrite, or type N to exit. ', (answer) => {
-      if (answer === 'Y') {
-        rl.close();
-        saveFile();
-      } else {
-        rl.close();
-      }
-    });
-  } else {
-    saveFile();
-  }
-});
+if (fileName.includes('./') && fileName.includes('.html')) {
+  
+  //Set up readline to allow userinput for file overwrite
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  fs.access(fileName, fs.constants.R_OK, (err) => {
+    if (!err) {
+      rl.question('This file name already exists. Type Y to overwrite, or type N to exit. ', (answer) => {
+        if (answer === 'Y') {
+          rl.close();
+          saveFile();
+        } else {
+          rl.close();
+        }
+      });
+    } else {
+      saveFile();
+      rl.close();
+    }
+  });
+}
+

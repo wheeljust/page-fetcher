8458f959082dd5856e793a5327096a9dd7fce659
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-19 00:31:46 +0000

    adds option to overwrite file if file already exists

--- fetcher.js
+++ fetcher.js
@@ -1,17 +1,52 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
 
+//Set up readline to allow userinput for file overwrite
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+//Arguments entered by the user will be in the order of 'node fetcher.js 'link' ./filelocation
 const link = process.argv[2];
 const fileName = process.argv[3];
 
-request(link, (error, response, body) => {
-  fs.writeFile(fileName, body, err => {
-    if (err) {
-      console.error(err)
-      return
+const saveFile = () => {
+  request(link, (error, response, body) => {
+    if (error) {
+      console.log(`An error has occured with the link. File not written.`);
+      process.exit();
     }
-  })
 
-  console.log(`Downloaded and saved ${body.length} bytes to ${fileName}`);
+    if(response.statusCode !== 200) {
+      console.log(`An error has occured with the link. Status code: ${response.statusCode}. File not written.`);
+      process.exit();
+    }
+
+    fs.writeFile(fileName, body, err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+
+    console.log(`Downloaded and saved ${body.length} bytes to ${fileName}`);
+  });
+};
+
+// Check for file name, if found prompt the user to input Y yto overwrite the file.
+fs.access(fileName, fs.constants.R_OK, (err) => {
+  if (!err) {
+    rl.question('This file name already exists. Type Y to overwrite, or type N to exit. ', (answer) => {
+      if (answer === 'Y') {
+        rl.close();
+        saveFile();
+      } else {
+        rl.close();
+      }
+    });
+  } else {
+    saveFile();
+  }
 });
-
